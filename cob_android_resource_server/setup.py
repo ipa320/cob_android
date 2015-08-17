3833aaaa172954b48cb016cc241e2173b27c6c6d
Benjamin Maidel <bnm@ipa.fgh.de>
2015-08-17 15:39:04 +0200

    fixed build errors

--- cob_android_resource_server/setup.py
+++ cob_android_resource_server/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
    packages=['cob_android_resource_server'],
    package_dir={'': 'src'},
-   scripts['scripts/resource_server'],
+   scripts=['scripts/resource_server'],
    requires=['rospkg']
 )
 
--- cob_android_script_server/setup.py
+++ cob_android_script_server/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
    packages=['cob_android_script_server'],
    package_dir={'': 'src'},
-   scripts['scripts/script_server_android'],
+   scripts=['scripts/script_server_android'],
    requires=['rospkg']
 )
 
