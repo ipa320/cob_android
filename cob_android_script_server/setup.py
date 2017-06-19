2b9e05b385c034b9f4b3af60034beb7203896483
Benjamin Maidel <bnm@ipa.fhg.de>
2017-06-19 16:04:57 +0200

    removed scripts and requires tag from setup.py

--- cob_android_script_server/setup.py
+++ cob_android_script_server/setup.py
@@ -6,8 +6,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
    packages=['cob_android_script_server'],
    package_dir={'': 'src'},
-   scripts=['scripts/script_server_android'],
-   requires=['rospkg']
 )
 
 setup(**d)
